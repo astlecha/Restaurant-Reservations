53340fb11b06e4f071900037aa7a73fa28db202d
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-07-20 14:03:54 -0700

    changes click handlers

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ var waitlist = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "homepage.html"));
 });
 
 app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "table.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function(req, res) {
